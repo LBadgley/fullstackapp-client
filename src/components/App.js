98f763df9d0cb3d4e6a07272ec9d4cd2fe08fb38
Laura Badgley <l.badgleyy@gmail.com>
2019-06-05 14:03:25 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Header from './Header';
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Router>
+        <Switch>
+          <Route />
+          <Route />
+        </Switch>
+      </Router>
+    </>
+  )
+}
